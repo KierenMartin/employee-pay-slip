e64bc64517079702dbb4a0ad017303048b7b89e7
KierenMartin <88122831+KierenMartin@users.noreply.github.com>
2021-07-29 14:09:38 +1000

    Hello World test

---
+++ src/employeepayslip/PaySlip.java
@@ -0,0 +1,13 @@
+
+package employeepayslip;
+
+import java.io.*;
+
+/**
+ * The main class.
+ */
+class PaySlip {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+}
